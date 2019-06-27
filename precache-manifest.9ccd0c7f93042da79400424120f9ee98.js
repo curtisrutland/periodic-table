d9ab72c2530cd34e13dd3a438f88ea83b403a97c
Curtis Rutland <curtisrutland@gmail.com>
2019-06-26 22:36:21 -0500

    Updates

---
+++ precache-manifest.9ccd0c7f93042da79400424120f9ee98.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41ba6e48d1ec6285af8b0625ceffaf4a",
+    "url": "/curtisrutland/periodic-table/index.html"
+  },
+  {
+    "revision": "e7b2654ba9cbfe5bb80f",
+    "url": "/curtisrutland/periodic-table/static/css/main.392f7eb2.chunk.css"
+  },
+  {
+    "revision": "1e5ffd494628c18c2e02",
+    "url": "/curtisrutland/periodic-table/static/js/2.b5954569.chunk.js"
+  },
+  {
+    "revision": "e7b2654ba9cbfe5bb80f",
+    "url": "/curtisrutland/periodic-table/static/js/main.aced8854.chunk.js"
+  },
+  {
+    "revision": "24740187184348049d69",
+    "url": "/curtisrutland/periodic-table/static/js/runtime~main.480d4be6.js"
+  }
+]);
